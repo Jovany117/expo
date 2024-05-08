bce215cdcc39279508c285b77835b5c9ae9200bb
Jovany117 <82356396+Jovany117@users.noreply.github.com>
2024-05-08 09:18:42 -0600

    Add files via upload

---
+++ js/funciones.js
@@ -0,0 +1,9 @@
+function abrir(){
+    document.getElementById("persona_1")
+    .style.height="250px";
+}
+
+function cerrar(){
+    document.getElementById("persona_1")
+    .style.height="35px";
+}
